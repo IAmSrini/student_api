384073dba2332be1e03e2642b34b7d3f77309b00
Srinivas <srinivas6692@gmail.com>
2016-03-30 02:22:06 -0400

    Changed test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,3 +8,23 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActiveRecord::FixtureSet
+  class << self
+    alias :orig_create_fixtures :create_fixtures
+  end
+  def self.create_fixtures f_dir, fs_names, *args
+    # Delete all fixtures that have foreign keys, in an order that
+    # doesn't break referential integrity.
+    Membership.delete_all
+
+    reset_cache
+
+    # If we're adding any {user, group} fixtures, add them [a] in that
+    # order, [b] before adding any other fixtures which might have
+    # references to them.
+    fs_names = %w(users groups) & fs_names | fs_names
+
+    orig_create_fixtures f_dir, fs_names, *args
+  end
+end
